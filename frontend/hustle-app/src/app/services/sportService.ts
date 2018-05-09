2d5325fe3f288231f8dd68618326e744cd87395a
Visko Plenković <vp48912@fer.hr>
2018-05-09 17:07:32 +0200

    [8]Get sports vol.4.

--- frontend/hustle-app/src/app/services/sportService.ts
+++ frontend/hustle-app/src/app/services/sportService.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Observable } from 'rxjs';
+import {LoginService} from "./login.service";
 import 'rxjs/add/operator/map'
 
 @Injectable()
 export class SportService {
-  constructor(private http:Http) {}
+  constructor(private http:Http,private loginService:LoginService) {}
 
   getSports(): Promise<any[]> {
-    return this.http.get('http://localhost:8080/api/sports')
+    let headers = new Headers({ 'Authorization': 'Bearer ' + this.loginService.token });
+    let options = new RequestOptions({ headers: headers });
+    return this.http.get('http://localhost:8080/api/sports',options)
       .toPromise()
       .then(response => response.json() as any[]);
   }
