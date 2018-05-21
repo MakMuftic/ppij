961357371559ff3fd6439abd409f95645afa502f
suchno <viskofrina@gmail.com>
2018-05-21 20:05:41 +0200

    fixes

--- frontend/hustle-app/src/app/Components/profile/profile.component.ts
+++ frontend/hustle-app/src/app/Components/profile/profile.component.ts
@@ -8,6 +8,8 @@ import { LoginService } from '../../services/login.service';
 import { User } from '../../models/user';
 import {init} from "protractor/built/launcher";
 import {SportService} from "../../services/sportService";
+import {Sport} from "../../models/sport";
+import {ImageService} from "../../services/imageService";
 
 @Component({
   selector: 'app-profile',
@@ -20,38 +22,35 @@ export class ProfileComponent implements OnInit {
   user:User;
   pass:string;
   pictureUrl:File;
-  selectedSports:number[];
+  selectedSports:Sport[];
   constructor(private router:Router,private userService:UserService,
-              private loginService:LoginService,private fb: FormBuilder,private sportService:SportService) {
+              private loginService:LoginService,private fb: FormBuilder,private sportService:SportService,
+              private imageService:ImageService) {
   }
 
   ngOnInit() {
-    this.initForms();
     this.sportService.getSports().then(
       sports => this.sports = sports
     );
     this.userService.getUser(JSON.parse(localStorage.getItem("currentUser")).id).then(
       user => this.user = user);
+    console.log(this.user.firstName);
 
   }
-  initForms() {
-    this.updateForm = this.fb.group({
-      'firstName': ['', Validators.required],
-      'lastName': ['', [Validators.required, Validators.minLength(4), Validators.maxLength(13)]],
-      'username': ['', [Validators.required]],
-      'email': ['', Validators.required],
-      'password': ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
-      'confirmPassword': ['', Validators.required],
-      'sports':[''],
-      'image':['']
-    });
-  }
+
   checkIfValidField(field: string,type:string): boolean {
     return !this.updateForm.controls[field].valid && this.updateForm.controls[field].touched;
 
   }
+  onFileChange(event) {
+    if(event.target.files.length > 0) {
+      let file = event.target.files[0];
+      this.pictureUrl=file;
+    }
+  }
   update() {
-    console.log(this.user.firstName);
-    console.log(this.selectedSports);
+    this.user.sports=this.selectedSports;
+    this.userService.updateUser(this.user);
+
   }
 }
