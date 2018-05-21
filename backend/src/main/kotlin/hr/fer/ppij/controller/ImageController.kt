b9ba70881a907dcc31c61d8c53b9c5ba563fe279
mace <mak.muftic@altima.hr>
2018-05-21 20:48:46 +0200

    Fix image controller

--- backend/src/main/kotlin/hr/fer/ppij/controller/ImageController.kt
+++ backend/src/main/kotlin/hr/fer/ppij/controller/ImageController.kt
@@ -6,6 +6,8 @@ import hr.fer.ppij.service.ImageService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import org.springframework.web.multipart.MultipartHttpServletRequest
+import java.util.ArrayList
 
 @ApiController
 class ImageController(
@@ -19,12 +21,28 @@ class ImageController(
         return ResponseEntity.notFound().build()
     }
 
-    @PostMapping("/images")
-    fun addImage(@RequestBody multipartFile: MultipartFile): ResponseEntity<*> {
-        if (!multipartFile.isEmpty) {
-            return ResponseEntity.ok(imageService.saveImage(multipartFile))
+    @PostMapping(value = ["/images"], consumes = ["multipart/form-data"])
+    fun addImage(request: MultipartHttpServletRequest): ResponseEntity<*> {
+
+        val images = getFiles(request)
+        
+        if (!images.isEmpty()) {
+            return ResponseEntity.ok(imageService.saveImage(images[0]))
         }
         return ResponseEntity.notFound().build()
+    }
+
+    private fun getFiles(request: MultipartHttpServletRequest): List<MultipartFile> {
+        val files = ArrayList<MultipartFile>()
+        val iterator = request.fileNames
+        var multipartFile: MultipartFile?
+        while (iterator.hasNext()) {
+            multipartFile = request.getFile(iterator.next())
+            if (multipartFile!!.originalFilename != "blob") {
+                files.add(multipartFile)
+            }
+        }
+        return files
     }
 
     @DeleteMapping("/images/{imageId}")
