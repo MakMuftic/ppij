81f6e3e20371d1d0ab0e7931adb899aca31bcfe5
mace <mak.muftic@altima.hr>
2018-05-21 23:52:29 +0200

    Fix register controller

--- backend/src/main/kotlin/hr/fer/ppij/controller/RegisterController.kt
+++ backend/src/main/kotlin/hr/fer/ppij/controller/RegisterController.kt
@@ -1,5 +1,7 @@
 package hr.fer.ppij.controller
 
+import hr.fer.ppij.model.Image
+import hr.fer.ppij.model.Sport
 import hr.fer.ppij.model.User
 import hr.fer.ppij.repository.UserRepository
 import org.springframework.web.bind.annotation.PostMapping
@@ -14,5 +16,38 @@ class RegisterController(
         private val userRepository: UserRepository
 ) {
     @PostMapping
-    fun registerUser(@RequestBody user: User): User = userRepository.save(user)
+    fun registerUser(@RequestBody registerWrapper: RegisterWrapper): User = userRepository.save(
+            User(
+                    0,
+                    registerWrapper.user.userName,
+                    registerWrapper.user.firstName,
+                    registerWrapper.user.lastName,
+                    registerWrapper.user.email,
+                    registerWrapper.password,
+                    registerWrapper.user.phoneNumber,
+                    registerWrapper.user.aboutMeDescription,
+                    registerWrapper.user.sports,
+                    registerWrapper.user.image,
+                    registerWrapper.user.admin
+            )
+    )
 }
+
+data class UserWrapper(
+        var id: Long,
+        var userName: String,
+        var firstName: String,
+        var lastName: String,
+        var email: String,
+        var phoneNumber: String,
+        var aboutMeDescription: String,
+        var sports: List<Sport>,
+        var image: Image,
+        var admin: Boolean
+)
+
+data class RegisterWrapper(
+        val user:UserWrapper,
+        val password: String
+)
+
